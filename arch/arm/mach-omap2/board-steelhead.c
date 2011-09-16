b87db03f8520aef772a21c2f0e9b29726dd51438
Mike J. Chen <mjchen@google.com>
2011-09-15 18:59:16 -0700

    ARM: omap4: steelhead: Incorporate recent changes from tuna
    This is manual merge of the following patches from tuna:
    
    commit fb078e6047a5e40b75eea869f56f61b09bee2662
    Author: Dmitry Shmidt <dimitrysh@google.com>
    Date:   Thu Sep 15 09:41:10 2011 -0700
    
        ARM: omap: tuna: Add CONFIG_CFG80211_ALLOW_RECONNECT=y
    
        Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    
    commit 109b128534b12b1827cdcdb50717a81d40ce5e61
    Author: Colin Cross <ccross@android.com>
    Date:   Wed Sep 14 18:09:01 2011 -0700
    
        ARM: omap4: tuna: add no_console_suspend to the command line
    
        no_console_suspend will allow panics that occur during suspend
        to be logged to ramconsole.
    
        Change-Id: Ia3f7018089dea6621fd2c1f9310bffebbb3ab79e
        Signed-off-by: Colin Cross <ccross@android.com>
    
    commit 8a70e7fbfca2a821f52d13cd30dc015bcec95ea9
    Author: Colin Cross <ccross@android.com>
    Date:   Wed Sep 14 16:46:02 2011 -0700
    
        ARM: omap4: tuna: add PL310 errata 727915 to defconfig
    
        Change-Id: I22a65856256dd89316538af6bfdd72ec433978e0
        Signed-off-by: Colin Cross <ccross@android.com>
    
    commit bea4fd0f8dbb81e1fc58489d6c2ecbf8039b5056
    Author: Lajos Molnar <molnar@ti.com>
    Date:   Wed Sep 7 15:32:43 2011 -0500
    
        TUNA:HDMI: set hpd_gpio and max pixel clock via board file.
    
        These are now configured via the board file.
    
        Change-Id: I3b4d60ff2b0f6f03be949837a45da602f3028fe3
        Signed-off-by: Lajos Molnar <molnar@ti.com>
    
    commit e04536e92ab7dd6a297a620282c910baec823544
    Author: Colin Cross <ccross@android.com>
    Date:   Sun Sep 11 18:25:29 2011 -0700
    
        ARM: omap4: tuna: increase vmalloc space to 768MB
    
        The default vmalloc space of 128MB results in 768MB of memory
        used as lowmem, which is a waste of address space.  Most memory
        allocations go to userspace, which doesn't care if the memory
        came from lowmem or highmem, and allocates pages individually,
        which will fragment lowmem.  Increase the vmalloc space to 768MB,
        which will reduce lowmem to 128 MB.
    
        Change-Id: Icd5ab94d5e93f627f93000bf6b44c78e3a1071e1
        Signed-off-by: Colin Cross <ccross@android.com>
    
    commit 92b5ddf886545b0f0cbb52813c72b6dac36529ea
    Author: Axel Castaneda Gonzalez <x0055901@ti.com>
    Date:   Thu Sep 8 21:59:34 2011 -0500
    
        ARM: omap4: tuna: Enable HDMI audio in defconfig
    
        Change-Id: I9f163661778cc8b6bb16c738f8ad43a3e50e8c38
        Signed-off-by: Axel Castaneda Gonzalez <x0055901@ti.com>
    
    Change-Id: I2b381ddc7b54564f86df52b49fe2f93208c0b76f
    Signed-off-by: Mike J. Chen <mjchen@google.com>

--- arch/arm/mach-omap2/board-steelhead.c
+++ arch/arm/mach-omap2/board-steelhead.c
@@ -1034,8 +1034,10 @@ static struct omap_dss_device  omap4_steelhead_hdmi_device = {
 		.hdmi	= {
 			.regn	= 15,
 			.regm2	= 1,
+			.max_pixclk_khz = 75000,
 		},
 	},
+	.hpd_gpio = GPIO_HDMI_HPD,
 	.channel = OMAP_DSS_CHANNEL_DIGIT,
 };
 
